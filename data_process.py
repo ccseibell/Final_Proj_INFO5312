82575102781f67b9f1d3bbadec37cc74e243d488
Clayton Seibel <claytonseibel@gmail.com>
2024-05-14 17:57:50 -0400

    5_14_work

--- data_process.py
+++ data_process.py
@@ -1,20 +1,17 @@
-# Import necessary packages
-from azureml.opendatasets import UsLaborLAUS
-import pandas as pd  # Ensure pandas is imported if used directly
 
-# Create an instance of UsLaborLAUS
+from azureml.opendatasets import UsLaborLAUS
+import pandas as pd ]
+
+
 usLaborLAUS = UsLaborLAUS()
 
-# Load the data into a pandas DataFrame
+
 usLaborLAUS_df = usLaborLAUS.to_pandas_dataframe()
 
-# Correct the DataFrame filtering to use the correct DataFrame variable
 usLaborLAUS_df = usLaborLAUS_df.loc[usLaborLAUS_df['measure_text'] == 'unemployment rate']
 
 grouped_df = usLaborLAUS_df.groupby(['srd_text', 'year'])['value'].mean()
 
-# Reset the index to turn the resulting Series back into a DataFrame
 grouped_df = grouped_df.reset_index()
 
-# Save the filtered DataFrame to a CSV file
 grouped_df.to_csv('usLaborLAUS.csv', index=False)
